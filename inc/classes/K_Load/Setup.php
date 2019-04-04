9b7c002584f02593b48ebd71c00d67c64f8cdf85
Sam <github@maddela.org>
2019-04-04 09:11:35 -0400

    again with the shitty hacks

--- inc/classes/K_Load/Setup.php
+++ inc/classes/K_Load/Setup.php
@@ -39,6 +39,7 @@ class Setup
         Cache::clear();
 
         \session_destroy();
+        unset($_SESSION);
 
         Util::log('action', 'K-Load has been installed', true);
         die('K-Load has been installed. Visit <a href="'.APP_PATH.'/dashboard/admin">'.APP_PATH.'/dashboard/admin</a>');
