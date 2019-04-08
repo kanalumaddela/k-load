4f7ba81da3da3f44330218cf6dc33cbf23f885ad
Sam <github@maddela.org>
2019-04-08 06:33:41 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- inc/classes/K_Load/Controller/Admin.php
+++ inc/classes/K_Load/Controller/Admin.php
@@ -12,6 +12,8 @@ use K_Load\Test;
 use K_Load\User;
 use K_Load\Util;
 use Steam;
+use const APP_ROOT;
+use const DIRECTORY_SEPARATOR;
 use function array_column;
 use function array_diff;
 use function array_intersect_key;
@@ -43,8 +45,6 @@ use function strtolower;
 use function substr;
 use function trim;
 use function unlink;
-use const APP_ROOT;
-use const DIRECTORY_SEPARATOR;
 
 class Admin
 {
